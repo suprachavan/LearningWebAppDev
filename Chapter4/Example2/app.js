274b808fb4f95ac8ffcb81db07d6b769226dd9bf
Supra Chavan <supra.chavan@gmail.com>
2016-09-26 16:55:01 -0700

    submission asgn 2 attempt 1

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,15 +1,29 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
+
+
 var main = function () {
     "use strict";
 
     var addCommentFromInputBox = function () {
         var $new_comment;
+        var newId1 = "user1";
+        var newId2 = "user2";
 
         if ($(".comment-input input").val() !== "") {
             $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
+            $new_comment.attr('id', newId1);
+            $new_comment.hide();  
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
             $(".comment-input input").val("");
+        }
+        if  ($(".comment-input2 input").val() !== "") {
+            $new_comment = $("<p>").text($(".comment-input2 input").val());
+            $new_comment.attr('id', newId2);
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input2 input").val("");
         }
     };
 
@@ -18,6 +32,16 @@ var main = function () {
     });
 
     $(".comment-input input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox();
+        }
+    });
+
+    $(".comment-input2 button").on("click", function (event) {
+        addCommentFromInputBox();
+    });
+
+    $(".comment-input2 input").on("keypress", function (event) {
         if (event.keyCode === 13) {
             addCommentFromInputBox();
         }
